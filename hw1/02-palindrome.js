3a44f485bd6562e5a26d0ed3fc6a93d35b672ea3
George Olson <golson@pdx.edu>
2023-09-28 17:09:25 -0700

    Find palindrome value;

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,21 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+
+function handleInput() {
+    console.log("Valid");
+    const value = document.querySelector('input').value;
+    valueString = value.toString();
+    valueReversed = valueString.split("").reverse().join("");
+    console.log("is " + valueString + " == " + valueReversed + "?");
+    
+    const result = document.getElementById('result');
+    if (valueString == valueReversed) {
+        result.innerText = "This is a palindrome."
+    } else {
+        result.innerText = "Not a palindrome."
+    }
+
+    return 0;
+};
