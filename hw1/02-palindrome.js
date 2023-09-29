06ef51f0ded562e6b43e9f24c8476f74b8f161e0
George Olson <golson@pdx.edu>
2023-09-28 17:13:05 -0700

    Check input for negative numbers & text;

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,16 +1,25 @@
 const elem = document.querySelector('input');
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener('input', checkPalindrome);
 
 
-function handleInput() {
-    console.log("Valid");
+function checkPalindrome() {
     const value = document.querySelector('input').value;
+    const result = document.getElementById('result');
+
+    if (isNaN(value)) {
+        result.innerText = "Input is not a number, please try again."
+        return 1;
+    }
+
+    if (value < 0) {
+        result.innerText = "Cannot use negative numbers, please try again."
+        return 1;
+    }
+
     valueString = value.toString();
     valueReversed = valueString.split("").reverse().join("");
-    console.log("is " + valueString + " == " + valueReversed + "?");
     
-    const result = document.getElementById('result');
     if (valueString == valueReversed) {
         result.innerText = "This is a palindrome."
     } else {
