93c7ecca053e7ff0ae8d6f4d2cbd64692a822eb5
George Olson <golson@pdx.edu>
2023-10-16 11:39:23 -0700

    Added functionality for toggling start & stop

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,17 +1,40 @@
 // Add your code here
 const form = document.querySelector('.background');
 const bodyElement = document.querySelector('body');
+const input = document.querySelector("input[name='seconds']");
 const colors = ["blue", "green", "purple", "red"];
+const button = document.querySelector('.submit');
+let interval = 0;
+
+const buttonStop = function buttonStop(changeToStop) {
+    if (changeToStop) {
+        button.classList.replace('btn-success', 'btn-danger');
+        button.value = 'Stop';
+    } else {
+        button.classList.replace('btn-danger', 'btn-success');
+        button.value = 'Start';
+    }
+}
 
 const handleSubmit = function handleSubmit(event) {
     event.preventDefault();
     console.log("We here");
-    startInterval();
+    if (interval === 0) {
+        startInterval();
+    } else {
+        stopInterval();
+    }
 }
 
 const startInterval = function startInterval() {
     const i = 0;
-    setInterval(changeColor, 1000);
+    interval = setInterval(changeColor, input.value * 1000);
+    buttonStop(true);
+}
+
+const stopInterval = function stopInterval() {
+    clearInterval(interval);
+    buttonStop(false);
 }
 
 const changeColor = function changeColor() {
