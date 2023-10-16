4b0b86e695e82ef1b6cc8c68156b191e89f77a05
George Olson <golson@pdx.edu>
2023-10-16 12:29:14 -0700

    Fix interval setting when stopping

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,15 +1,27 @@
-// Add your code here
+// Declare values for elements on the background page
 const form = document.querySelector('.background');
 const bodyElement = document.querySelector('body');
 const input = document.querySelector("input[name='seconds']");
 const button = document.querySelector('.submit');
+
+// Declare initial interval value at 0 so we know if the value has changed when toggling settings
 let interval = 0;
 
 // https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
+// Random function to select value between min and max
 const randomBetween = function randomBetween(min, max) {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
 
+// Function to generate a random RGBA code for color
+const randomColor = function randomColor() {
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgba(${r}, ${g}, ${b}, 0.75)`;
+}
+
+// Function that toggles values on button between starting and stopping background color change intervals
 const buttonStop = function buttonStop(changeToStop) {
     if (changeToStop) {
         button.classList.replace('btn-success', 'btn-danger');
@@ -17,36 +29,39 @@ const buttonStop = function buttonStop(changeToStop) {
     } else {
         button.classList.replace('btn-danger', 'btn-success');
         button.value = 'Start';
+        interval = 0;
     }
 }
 
-const handleSubmit = function handleSubmit(event) {
-    event.preventDefault();
-    console.log("We here");
-    if (interval === 0) {
-        startInterval();
-    } else {
-        stopInterval();
-    }
+// Function that changes the color of the background element
+const changeColor = function changeColor() {
+    console.log("Change color");
+    bodyElement.style.background = randomColor();
 }
 
+// Function that begins interval based on form's seconds value and multiplies by 1000 to convert to millisecond value
 const startInterval = function startInterval() {
     const i = 0;
     interval = setInterval(changeColor, input.value * 1000);
     buttonStop(true);
 }
 
+// Stops interval
 const stopInterval = function stopInterval() {
     clearInterval(interval);
     buttonStop(false);
 }
 
-const changeColor = function changeColor() {
-    console.log("Change color");
-    const r = randomBetween(0, 255);
-    const g = randomBetween(0, 255);
-    const b = randomBetween(0, 255);
-    bodyElement.style.background = `rgba(${r}, ${g}, ${b}, 0.75)`;
+// Function that handles the submit event
+const handleSubmit = function handleSubmit(event) {
+    event.preventDefault();
+    console.log("We here");
+    if (interval === 0) {
+        bodyElement.style.background = randomColor();
+        startInterval();
+    } else {
+        stopInterval();
+    }
 }
 
 form.addEventListener("submit", handleSubmit);
