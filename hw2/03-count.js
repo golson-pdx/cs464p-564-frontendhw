5257102792ffe133868452b894756db363d29b21
George Olson <golson@pdx.edu>
2023-10-15 20:37:13 -0700

    Added boundaries on both sides

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -9,8 +9,8 @@ const originalBodyText = body.innerHTML;
 const handleKeyDown = function handleKeyDown() {
     const inputValue = input.value;
 
-    // A regular expression to quickly search for matching substrings in the string. Word boundary is added at the end of the substring to limit search to full words and not just characters
-    const regEx = new RegExp(`${inputValue}\\b`, 'g');
+    // A regular expression to quickly search for matching substrings in the string. Word boundaries are added at the beginning & end of the substring to limit search to full words and not just characters
+    const regEx = new RegExp(`\\b${inputValue}\\b`, 'g');
 
     // Replace substrings in string with span containing highlight class and text inside
     body.innerHTML = originalBodyText.replaceAll(regEx, `<span class="highlight">${inputValue}</span>`);
