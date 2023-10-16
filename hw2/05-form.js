952847f5d90c00960372cbbd8df8ee06e3438a0e
George Olson <golson@pdx.edu>
2023-10-16 15:59:43 -0700

    Updated form to print out results in console, disable default option

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,35 @@
 // Add your code here
+const form = document.querySelector('form');
+
+const handleSubmit = function handleSubmit(event) {
+    event.preventDefault();
+    const name = form.elements.fullName.value;
+    const email = form.elements.email.value;
+    const registrationStatus = form.elements.registrationStatus.value;
+
+    const courseElements = form.elements.courses;
+    const courses = [];
+    const comments = form.elements.comments.value;
+
+    console.log(courseElements);
+
+    for (let i = 0; i < courseElements.length; i++) {
+        if (courseElements[i].checked) {
+            courses.push(courseElements[i].value);
+        }
+    }
+
+    let coursesString = "";
+    if (courses.length > 0) {
+        for (let i = 0; i < courses.length; i++) {
+            coursesString += `${courses[i]}`;
+            if (i+1 < courses.length) {
+                coursesString += `, `
+            }
+        }
+    }
+
+    console.log(`Full name: ${name}\nEmail: ${email}\nRegistration Status: ${registrationStatus}\nClasses Taken: ${coursesString}\nComments: ${comments}`);
+}
+
+form.addEventListener("submit", handleSubmit);
