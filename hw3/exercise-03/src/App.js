45959760aff42fd2241690146f8e73065ba24546
George Olson <golson@pdx.edu>
2023-11-01 10:26:49 -0700

    Update header links

--- hw3/exercise-03/src/App.js
+++ hw3/exercise-03/src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/home"
         >
-          Learn React
+          Home
+        </a>
+        <a
+          className="App-link"
+          href="/search"
+        >
+          Search
+        </a>
+        <a
+          className="App-link"
+          href="/houses"
+        >
+          Houses
         </a>
       </header>
     </div>
