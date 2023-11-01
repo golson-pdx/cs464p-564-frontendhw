d5364aa2997a8282f0d7904ab40b1c64bb57a698
George Olson <golson@pdx.edu>
2023-10-31 17:39:24 -0700

    add API request and create front end listing of characters

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,25 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const characterBody = document.querySelector('#characters');
 
-fetch(url);
+const data = fetch(url).then(response => {
+    //Show status of request in console
+        console.log('Request successful', response);
+        return response.json();
+    })
+    .then(data => {
+        //Loop through data 
+        data.forEach((data_item) => {
+            appendCharacter(data_item);
+        })
+
+    })
+    .catch(error => {
+        console.error('Request failed', error)
+    });
+
+
+const appendCharacter = function appendCharacterToBody(data_item) {
+    console.log(data_item);
+    characterBody.innerHTML += `<div class="col-6 text-center character"><img src="${data_item.imageUrl}" alt="${data_item.firstName} ${data_item.lastName}" class="img-fluid"/><h2>${data_item.firstName} ${data_item.lastName}</h2><span class="title">${data_item.title}</span></div>`
+}
